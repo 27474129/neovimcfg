131b03bba6f2b65be60a653aa9d80cf6bc36a671
Vladimir <71443706+27474129@users.noreply.github.com>
2023-11-21 05:37:10 -0500

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,2 +1,7 @@
 apt-get install silversearcher-ag
 pip3 install pynvim
+sudo apt install ripgrep
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+source ~/.config/nvim/init.vim
